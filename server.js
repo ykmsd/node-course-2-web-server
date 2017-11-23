c83bf39d8cc04398f7b67aec9ce5047a828dada8
Yuka <yuka1110m@gmail.com>
2017-11-23 10:48:09 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to sever.log.')
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express</h1>');
+  res.render('home.hbs', {
+    welcomeMessage: 'Hello my friend!',
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/about',(req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+// /bad - send back json with errorMessage
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle your request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.')
+});
